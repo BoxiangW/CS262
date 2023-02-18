3760d6cf8d292722edfb83f642a219ee841eaade
hos323 <hongyi_sun@fas.harvard.edu>
2023-02-18 10:31:43 -0500

    almost finished normal implementation

--- client.py
+++ client.py
@@ -1,5 +1,22 @@
 import socket
 import json
+import threading
+import time
+import sys
+
+# a function for printing to stderr
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+# a message generator function
+def create_msg(cmd, src="", to="", body=""):
+    msg = {
+        "cmd": cmd,
+        "from": src,
+        "to": to,
+        "body": body
+    }
+    return json.dumps(msg).encode()
 
 class ChatClient:
     def __init__(self, server_host, server_port):
@@ -7,50 +24,109 @@ class ChatClient:
         self.server_port = server_port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((server_host, server_port))
+        self.username = None
 
     def send_message(self, recipient, message):
-        request = {
-            "type": "message",
-            "recipient": recipient,
-            "message": message
-        }
-        self.sock.sendall(json.dumps(request).encode())
+        if not self.username:
+            eprint("Please create your username first")
+        else:
+            self.sock.sendall(create_msg("send", self.username, recipient, message))
 
-    def list_accounts(self, username=""):
-        request = {
-            "type": "list",
-            "username": username
-        }
-        self.sock.sendall(json.dumps(request).encode())
-        response = self.sock.recv(1024).decode()
-        return json.loads(response)
+    def list_accounts(self, wildcard):
+        self.sock.sendall(create_msg("list", self.username, body=wildcard))
 
     def create_account(self, username):
-        request = {
-            "type": "create",
-            "username": username
-        }
-        self.sock.sendall(json.dumps(request).encode())
-        response = self.sock.recv(1024).decode()
-        return json.loads(response)
+        self.sock.sendall(create_msg("create", username))
 
-    def delete_account(self, username):
-        request = {
-            "type": "delete",
-            "username": username
-        }
-        self.sock.sendall(json.dumps(request).encode())
-        response = self.sock.recv(1024).decode()
-        return json.loads(response)
+    def delete_account(self):
+        self.sock.sendall(create_msg("delete", self.username))
 
-    def receive_messages(self, username):
-        request = {
-            "type": "deliver",
-            "username": username
-        }
-        self.sock.sendall(json.dumps(request).encode())
-        response = self.sock.recv(1024).decode()
-        return json.loads(response)
+    def receive_messages(self):
+        self.sock.sendall(create_msg("deliver", self.username))
 
     def close(self):
+        self.sock.sendall(create_msg("close", self.username))
         self.sock.close()
+
+
+# Interactive script below
+PORT=56789
+
+# Create an instance of the ChatClient class
+client = ChatClient("localhost", PORT)
+
+# handles user interaction
+def handle_user():
+    # Loop until the user chooses to exit
+    while True:
+        # Print the available commands
+        print("Available commands:")
+        print("1. Send a message")
+        print("2. List accounts")
+        print("3. Create an account")
+        print("4. Delete an account")
+        print("5. Exit")
+
+        # Prompt the user for input
+        choice = input("Enter a command number (1-6): ")
+
+        # Call the appropriate client function based on the user input
+        if choice == "1":
+            recipient = input("Enter the recipient's username: ")
+            message = input("Enter the message: ")
+            client.send_message(recipient, message)
+        elif choice == "2":
+            username = input("Enter a username (optional): ")
+            client.list_accounts(username)
+        elif choice == "3":
+            username = input("Enter the username to create: ")
+            client.create_account(username)
+        elif choice == "4":
+            client.delete_account()
+        elif choice == "5":
+            client.close()
+            break
+        else:
+            print("Invalid command. Please try again.")
+
+def handle_message():
+    while True:
+        msg = client.sock.recv(1024).decode()
+        if not msg:
+            break
+        msg = json.loads(msg)
+
+        if msg["cmd"] == "deliver":
+            print("{} sent: {}".format(msg["from"], msg["body"]))
+        elif msg["cmd"] == "create":
+            if msg["error"]:
+                print("Failed to create account: {}. Please try again.".format(msg["body"]))
+            else:
+                print("Account created successfully.")
+                client.username = msg["to"]
+        elif msg["cmd"] == "delete":
+            if msg["error"]:
+                print("Failed to delete account: {}. Please try again.".format(msg["body"]))
+            else:
+                print("Account deleted successfully.")
+                client.username = None
+        elif msg["cmd"] == "list":
+            print(json.dumps(msg["body"], indent=2))
+        elif msg["cmd"] == "send":
+            if msg["error"]:
+                print("Failed to send message: {}. Please try again.".format(msg["body"]))
+            else:
+                print(msg["body"])
+
+# this function requests for a msg delivery every 5 sec
+def request_deliver():
+    while True:
+        if client.username:
+            client.receive_messages()
+            time.sleep(5.0)
+
+
+if __name__ == '__main__':
+    threading.Thread(target=handle_user).start()
+    threading.Thread(target=handle_message).start()
+    threading.Thread(target=request_deliver).start()
--- server.py
+++ server.py
@@ -5,23 +5,50 @@ import threading
 
 # define host and port for server
 HOST = 'localhost'
-PORT = 55555
+PORT = 56789
+
+# define the maximum message size
+MSGLEN = 4096
 
 # create a dictionary to store user accounts and their undelivered messages
 users = {}
 
-# json encoded message: {"cmd": ..., "from": .., "to": ..., "body": ...}
+# json encoded message: {"cmd": ..., "from": .., "to": ..., "body": ..., "err": ...}
 
-# define a function to handle client connections
+# a message generator function
+def create_msg(cmd, src="", to="", body = "", err=False):
+    msg = {
+        "cmd": cmd,
+        "from": src,
+        "to": to,
+        "body": body,
+        "error": err
+    }
+    return json.dumps(msg).encode()
+
+# a message receiver, according to <https://docs.python.org/3/howto/sockets.html>
+def receive(conn):
+    # chunks = []
+    # bytes_recd = 0
+    # while bytes_recd < MSGLEN:
+    #     chunk = conn.recv(min(MSGLEN - bytes_recd, 2048))
+    #     if chunk == b'':
+    #         raise RuntimeError("socket connection broken")
+    #     chunks.append(chunk)
+    #     bytes_recd = bytes_recd + len(chunk)
+    # return b''.join(chunks)
+    return conn.recv(MSGLEN)
+
+# a function to handle client connections
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
 
     while True:
         # receive message from client
-        msg = conn.recv(1024).decode("utf-8")
+        msg = receive(conn).decode()
 
         # check if message is empty or the client has disconnected
-        if not msg or msg == "disconnect":
+        if not msg:
             print(f"[DISCONNECT] {addr} disconnected.")
             break
 
@@ -34,19 +61,20 @@ def handle_client(conn, addr):
         if cmd == "create":
             # check if username already exists
             if username in users:
-                conn.send("error|Username already exists".encode())
+                conn.send(create_msg(cmd, body="Username already exists", err=True))
             else:
                 # create new user account
                 users[username] = []
-                conn.send("success|Account created".encode())
+                conn.send(create_msg(cmd, body="Account created", to=username))
 
         # handle list accounts command
         elif cmd == "list":
             # create a list of usernames matching the wildcard (if any)
-            wildcard = parts["body"] if len(parts) > 3 else ""
+            wildcard = parts["body"] if parts["body"] else '*'
             matching_users = fnmatch.filter(list(users.keys()), wildcard)
             # send list of usernames to client
-            conn.send("list|" + ",".join(matching_users).encode())
+            matching_users = ",".join(matching_users)
+            conn.send(create_msg(cmd, body=matching_users))
 
         # handle send message command
         elif cmd == "send":
@@ -54,38 +82,43 @@ def handle_client(conn, addr):
             message = parts["body"]
             # check if recipient is a registered user
             if recipient not in users:
-                conn.send("error|Recipient not found".encode())
+                conn.send(create_msg(cmd, body="Recipient not found", err=True))
             else:
                 # add message to recipient's message queue
                 users[recipient].append((username, message))
                 # notify client that message was sent
-                conn.send("success|Message sent".encode())
+                conn.send(create_msg(cmd, body="Message sent"))
 
         # handle deliver messages command
         elif cmd == "deliver":
-            # retrieve undelivered messages for this user
-            messages = users.get(username, [])
-            # send messages to client
-            for (sender, message) in messages:
-                conn.send(f"message|{sender}|{message}".encode())
-            # clear user's message queue
-            users[username] = []
+            if username not in users:
+                conn.send(create_msg(cmd, body="User not found", err=True))
+            else:
+                # retrieve undelivered messages for this user
+                messages = users.get(username, [])
+                # send messages to client
+                for (sender, message) in messages:
+                    conn.send(create_msg(cmd, src=sender, body=message))
+                # clear user's message queue
+                users[username] = []
 
         # handle delete account command
         elif cmd == "delete":
             # check if user has undelivered messages
-            if username in users and len(users[username]) > 0:
-                conn.send("error|Undelivered messages exist".encode())
+            if username not in users:
+                conn.send(create_msg("error", body="User does not exist"))
+            elif username in users and len(users[username]) > 0:
+                conn.send(create_msg("error", body="Undelivered messages exist"))
             else:
                 # delete user account
-                if username in users:
-                    del users[username]
+                del users[username]
                 # notify client that account was deleted
-                conn.send("success|Account deleted".encode())
+                conn.send(create_msg("success", body="Account deleted"))
                 # disconnect client
-                conn.send("disconnect".encode())
-                conn.close()
-                break
+                # conn.send(create_msg("error", body="disconnect"))
+        elif cmd == "close":
+            print(f"[DISCONNECT] {addr} disconnected.")
+            break
 
     conn.close()
 
