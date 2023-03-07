e057e60f3efce72f53161f32d1e0f6e4e15044b7
Boxiang Wang <bwang1@fas.harvard.edu>
2023-03-06 20:29:33 -0500

    Modified VM to process and logged 5 times

---
+++ hw2/notebook.md
@@ -0,0 +1,47 @@
+# Engineering Notebook
+
+## Decisions
+
+1. We first decided to open and close the socket for every message, but we quickly realized that this is very inefficient becuase opening a socket requires 3 packet exchanges, and closing it requires 4. Therefore we decided to keep the socket open.
+
+2. We defined the VirtualMachine class as a thread as the beginning, but we realized that to make the VM perform send and receive at the same time, we need to make the VM a process. Therefore we changed the VM to a process.
+
+3. We planned to use different threads for sending messages and do internal events, but we realized that with this setting it might be difficult to synchronize the two threads. Therefore we decided to use a single thread for both sending messages and doing internal events, as named as tick.
+
+## Structure
+
+- `VirtualMachine`:
+
+    A Class inherited from Process. It uses two threads to continusly receive messages and tick. It stores multiply sockets for different clients.
+
+  - `__init__(self, name, host, port, clock_rate, vm_address_list)`:
+
+    Initialize the VM. It opens a log file in overwrite mode.
+
+  - `run(self)`:
+
+    The main function of the VM. It creates two threads, one for receiving messages with `target=self.receive_thread` and one for ticking with `target=self.tick_thread`. It overide the run method of multiprocessing.Process. See details at [here](https://docs.python.org/3/library/multiprocessing.html#multiprocessing.Process.run).
+
+  - `receive_thread(self)`:
+
+    Create a socket and listen to the port. After accepting a connection, it will create a new thread with `target = self.receive_message` to handle the connection.
+
+  - `receive_message(self, conn)`:
+  
+    After a connection is established, it will receive the message and put it into the message queue.
+
+  - `tick_thread(self)`:
+
+    Create two sockets for sending messages to two other VMs. It will tick every 1/clock_rate seconds. After ticking, it will call `tick()` to operate.
+
+  - `tick(self)`:
+
+    It will check the message queue and do the corresponding operations. If the message queue is not empty, take one message off the queue and process it. If the message queue is empty, generate a random number from 1 to 10. For number 1, send its local logical clock time to one VM. For number 2, send its local logical clock time to the other VM. For number 3, send its local logical clock time to both VMs. Otherwise, do nothing. For all the above, log its operation, system time, and local logical clock time, if message queue is not empty, also log the message queue length.
+  
+  - `log(self, message)`:
+
+    Helper to log the message to the log file.
+
+## Findings
+
+TODO
--- hw2/virtualmachine.py
+++ hw2/virtualmachine.py
@@ -1,47 +1,141 @@
-import multiprocessing
+from multiprocessing import Process
+from threading import Thread
+from _thread import *
 import random
 import time
 import socket
 import datetime
 
 
-class VirtualMachine(multiprocessing.Process):
-    """Virtual machine for hw 2.
+class VirtualMachine(Process):
+    """Process representing a single virtual machine for hw 2.
 
     Attributes:
         name: A string indicates the name.
         host: A string indicates the host.
         port: An integer indicates the port.
+        clock_rate: An integer indicates the clock rate.
+        vm_address_list: A list of tuples containing addresses.
         message_queue: A queue for storing messages.
-        vm_list: A list of VirtualMachine instances.
         logical_clock: An integer indicates the logical clock time.
         log_file: A string indicates the log file name.
-        receiver: A socket instance for receiving messages.
     """
 
-    def __init__(self, name, host, port, vm_list):
+    def __init__(self, name, host, port, clock_rate, vm_address_list):
         """Initializes the instance with basic settings.
 
         Args:
           name: Used to identify the VM.
           host: Used to connect to the VM.
           port: Used to connect to the VM.
-          vm_list: Used to send messages to other VMs.
+          clock_rate: Used to control the speed of the VM.
+          vm_address_list: Used to send messages to other VMs.
         """
-        multiprocessing.Process.__init__(self)
+        Process.__init__(self)
         self.name = name
         self.host = host
         self.port = port
+        self.clock_rate = clock_rate
+        self.vm_address_list = vm_address_list
+        self.sender = []
         self.message_queue = []
-        self.vm_list = vm_list
         self.logical_clock = 0
         self.log_file = f"{self.name}_log.txt"
         open(self.log_file, 'w')
         self.log(f"=============LOG START=============\n")
+        self.log(f"clock_rate = {clock_rate}\n")
 
-    # def __del__(self):
-    #     """Delete current instance."""
-    #     self.log_file.close()
+    def run(self):
+        """Method representing the process's activity.
+
+        Overide the run method of multiprocessing.Process. See details at
+        https://docs.python.org/3/library/multiprocessing.html#multiprocessing.Process.run
+        """
+        # start a thread to receive messages
+        Thread(target=self.receive_thread).start()
+
+        # wait for all VMs to start otherwise the VMs might not be able to connet
+        time.sleep(1)
+
+        # send messages
+        Thread(target=self.tick_thread).start()
+
+    def receive_thread(self):
+        """Thread for receiving messages."""
+        # create an INET, STREAMing socket
+        receiver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        receiver.bind((self.host, self.port))
+        receiver.listen(2)
+        print(f"{self.name}: Listening on {self.host}:{self.port}")
+
+        # accept connections from outside
+        while True:
+            conn, address = receiver.accept()
+            Thread(target=self.receive_message, args=(conn,)).start()
+
+    def receive_message(self, conn):
+        """Receive message from other VMs.
+
+        Args:
+          conn: Socket connection.
+        """
+        while True:
+            data = conn.recv(1024).decode()
+            if data:
+                self.message_queue.append(data)
+
+    def tick_thread(self):
+        """Thread for ticking the VM."""
+        # create a socket for each VM
+        for _ in range(len(self.vm_address_list)):
+            self.sender.append(socket.socket(
+                socket.AF_INET, socket.SOCK_STREAM))
+
+        # connect to other VMs
+        try:
+            for i in range(len(self.vm_address_list)):
+                self.sender[i].connect(
+                    (self.vm_address_list[i][0], self.vm_address_list[i][1]))
+
+        except socket.error as e:
+            print("Error connecting sender: %s" % e)
+
+        while True:
+            starttime = time.time()
+            self.tick()  # tick the VM
+            endtime = time.time()
+            elapsed = endtime - starttime
+            time.sleep((1 / self.clock_rate) - elapsed)
+
+    def tick(self):
+        """Method representing the VM's activity."""
+
+        # if the message queue is not empty, take one message off the queue and process it
+        if len(self.message_queue):
+            self.logical_clock = max(
+                self.logical_clock, int(self.message_queue[0])) + 1
+            data = self.message_queue.pop(0)
+            log_message = f"[Receive]: {data}, [System time]: {datetime.datetime.now()}, [Message Queue Length]: {len(self.message_queue)}, [Logical Clock Time]: {self.logical_clock}\n"
+            self.log(log_message)
+
+        # if the message queue is empty, send message or do internal event
+        else:
+            event = random.randint(1, 10)
+            self.logical_clock += 1
+            if event <= 3:  # send message
+                if event == 1:  # send message to one VM
+                    self.sender[0].send(str(self.logical_clock).encode())
+                elif event == 2:  # send message to the other VM
+                    self.sender[1].send(str(self.logical_clock).encode())
+                elif event == 3:  # send message to both VM
+                    for i in range(len(self.sender)):
+                        self.sender[i].send(str(self.logical_clock).encode())
+                log_message = f"[Send]: {self.logical_clock}, [System time]: {datetime.datetime.now()}, [Logical Clock Time]: {self.logical_clock}\n"
+                self.log(log_message)
+
+            else:  # do internal event
+                log_message = f"[Internal event], [System time]: {datetime.datetime.now()}, [Logical Clock Time]: {self.logical_clock}\n"
+                self.log(log_message)
 
     def log(self, message):
         """Logs the message to the log file.
@@ -52,97 +146,8 @@ class VirtualMachine(multiprocessing.Process):
         with open(self.log_file, 'a+') as logfile:
             logfile.write(message)
 
-    def run(self):
-        """Method representing the process's activity.
-
-        Overide the run method of multiprocessing.Process. See details at
-        https://docs.python.org/3/library/multiprocessing.html#multiprocessing.Process.run
-        """
-        # create an INET, STREAMing socket
-        self.receiver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.receiver.bind((self.host, self.port))
-        self.receiver.listen(5)
-        print(f"{self.name}: Listening on {self.host}:{self.port}")
-
-        # accept connections from outside
-        while True:
-            client, address = self.receiver.accept()
-            data = client.recv(1024).decode()
-            if data:
-                self.message_queue.append(data)
-            client.close()
-
-    def send_message(self, target):
-        """Send local logical clock time to the target VM.
-
-        Args:
-          target: VirtualMachine instance to send the message to.
-        """
-        # create an INET, STREAMing socket
-        sender = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sender.connect((target.host, target.port))
-        sender.sendall(str(self.logical_clock).encode())
-        sender.close()
-
-    def tick(self):
-        """Method representing the VM's activity."""
-
-        # if the message queue is not empty take one message off the queue and process it
-        if len(self.message_queue):
-            data = self.message_queue.pop(0)
-            # update logical clock
-            # self.logical_clock = max(self.logical_clock, int(data)) + 1
-            self.logical_clock += 1
-            log_message = f"[Receive]: {data}, [System time]: {datetime.datetime.now()}, [Message Queue Length]: {len(self.message_queue)}, [Logical Clock Time]: {self.logical_clock}\n"
-            self.log(log_message)
-
-        else:  # if the message queue is empty, send message or do internal event
-            event = random.randint(1, 10)
-            self.logical_clock += 1
-
-            if event <= 3:  # send message
-                if event == 1:  # send message to one VM
-                    target = self.vm_list[0]
-                    self.send_message(target)
-                elif event == 2:  # send message to the other VM
-                    target = self.vm_list[1]
-                    self.send_message(target)
-                elif event == 3:  # send message to both VM
-                    for target in self.vm_list:
-                        self.send_message(target)
-                log_message = f"[Send]: {self.logical_clock}, [System time]: {datetime.datetime.now()}, [Logical Clock Time]: {self.logical_clock}\n"
-                self.log(log_message)
-
-            else:  # do internal event
-                log_message = f"[Internal event], [System time]: {datetime.datetime.now()}, [Logical Clock Time]: {self.logical_clock}\n"
-                self.log(log_message)
-
-
-def handle_tick(vm, clockrate):
-    """VM tick handler for threading.
-
-    Args:
-        vm: VirtualMachine instance to be ticked.
-        clockrate: clock rate of the VM.
-    """
-    while True:
-        vm.tick()  # tick the VM
-        time.sleep(1 / clockrate)  # sleep for 1/clockrate seconds
-
 
 if __name__ == '__main__':
-
-    # init VMs
-    vm1 = VirtualMachine("VM1", "localhost", 8000, [])
-    vm2 = VirtualMachine("VM2", "localhost", 8001, [])
-    vm3 = VirtualMachine("VM3", "localhost", 8002, [])
-    vm1.vm_list = [vm2, vm3]
-    vm2.vm_list = [vm1, vm3]
-    vm3.vm_list = [vm1, vm2]
-    vm1.start()
-    vm2.start()
-    vm3.start()
-
     # set clock rates for each VM
     vm1_clockrate = random.randint(1, 6)
     vm2_clockrate = random.randint(1, 6)
@@ -151,10 +156,25 @@ if __name__ == '__main__':
     print("VM2 clock rate: ", vm2_clockrate)
     print("VM3 clock rate: ", vm3_clockrate)
 
-    # start processes
-    multiprocessing.Process(
-        target=handle_tick, args=(vm1, vm1_clockrate)).start()
-    multiprocessing.Process(
-        target=handle_tick, args=(vm2, vm2_clockrate)).start()
-    multiprocessing.Process(
-        target=handle_tick, args=(vm3, vm3_clockrate)).start()
+    vm1_address, vm1_port = "localhost", 10113
+    vm2_address, vm2_port = "localhost", 10114
+    vm3_address, vm3_port = "localhost", 10115
+
+    # init VMs
+    vm1 = VirtualMachine("VM1", "localhost", 10113, vm1_clockrate, [
+                         (vm2_address, vm2_port), (vm3_address, vm3_port)])
+    vm2 = VirtualMachine("VM2", "localhost", 10114, vm2_clockrate, [
+                         (vm3_address, vm3_port), (vm1_address, vm1_port)])
+    vm3 = VirtualMachine("VM3", "localhost", 10115, vm3_clockrate, [
+                         (vm1_address, vm1_port), (vm2_address, vm2_port)])
+
+    vm1.start()
+    vm2.start()
+    vm3.start()
+
+    time.sleep(60)  # run for 1 minutes
+
+    # wait for processes to finish
+    vm1.terminate()
+    vm2.terminate()
+    vm3.terminate()
